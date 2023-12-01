5dc1500e7fb400a3544d13becb8b87e51d1ef3c2
jagnani73 <yashjagnani73@gmail.com>
2023-12-01 11:20:24 +0530

    fix: eslint prettier issues

--- src/components/Organisms/TokenBalances/AddressActivityListView/AddressActivityListView.tsx
+++ src/components/Organisms/TokenBalances/AddressActivityListView/AddressActivityListView.tsx
@@ -323,10 +323,10 @@ export const AddressActivityListView: React.FC<
                                         {header.isPlaceholder
                                             ? null
                                             : flexRender(
-                                                header.column.columnDef
-                                                    .header,
-                                                header.getContext()
-                                            )}
+                                                  header.column.columnDef
+                                                      .header,
+                                                  header.getContext()
+                                              )}
                                     </TableHead>
                                 );
                             })}
