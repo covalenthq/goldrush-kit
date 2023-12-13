9718f95b73534a0f2b44705c5b9674d8ee995b4a
nguyenlejoe <joe_le.1@hotmail.com>
2023-12-12 17:11:56 -0800

    add exports for release

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,11 @@ export { AddressActivityListView } from "./components/Organisms/TokenBalances/Ad
 export { TokenBalancesListView } from "./components/Organisms/TokenBalances/TokenBalancesListView/TokenBalancesListView";
 export { TokenTransfersListView } from "./components/Organisms/TokenBalances/TokenTransfersListView/TokenTransfersListView";
 export { NFTWalletTokenListView } from "./components/Organisms/NFTs/NFTWalletTokenListView/NFTWalletTokenListView";
+export { NFTCollectionTokenListView } from "./components/Organisms/NFTs/NFTCollectionTokenListView/NFTCollectionTokenListView";
+export { type AddressActivityListViewProps } from "./utils/types/organisms.types";
+export { type TokenBalancesListViewProps } from "./utils/types/organisms.types";
+export { type TokenTransfersListViewProps } from "./utils/types/organisms.types";
+export { type NFTWalletTokenListViewProps } from "./utils/types/organisms.types";
+export { type NFTCollectionTokenListViewProps } from "./utils/types/organisms.types";
 export { CovalentProvider as GoldRushProvider } from "./utils/store/Covalent";
+export { useCovalent } from "./utils/store/Covalent"
