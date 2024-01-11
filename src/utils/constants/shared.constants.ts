26258d3a8f148bdc194a9d3201d74b84acc20728
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2024-01-11 15:40:34 +0530

    feat: decoded tx

--- src/utils/constants/shared.constants.ts
+++ src/utils/constants/shared.constants.ts
@@ -8,10 +8,6 @@ export enum GRK_SIZES {
     EXTRA_EXTRA_SMALL = "xxs",
 }
 
-export type GRK_NFT_SIZES = "lg" | "md" | "sm" | "default";
-
-export type GRK_WALLET_TYPES = "effigy" | "fingerprint" | "wallet";
-
 export const CHART_COLORS: Color[] = [
     "slate",
     "violet",
@@ -365,3 +361,27 @@ export const allowedCacheChains = [
     "matic-mainnet",
     "matic-mumbai",
 ];
+
+export enum DECODED_EVENT_CATEGORY {
+    NFT = "NFT Transaction",
+    LENDING = "Lending",
+    SAFE = "SAFE",
+    DEX = "DEX",
+    TOKEN = "Token",
+    SWAP = "Swap",
+    MINT = "Mint",
+    DEFI = "DeFi",
+    BRIDGE = "Bridge",
+    GAMING = "Gaming",
+    SOCIAL = "Social",
+    OTHERS = "Others",
+}
+
+export enum DECODED_ACTION {
+    SWAPPED = "Swapped",
+    MULTISIG_ACTION = "MultiSig",
+    APPROVAL = "Approval",
+    TRANSFERRED = "Transferred",
+    RECEIVED_BRIDGE = "Received Bridge",
+    ACCOUNT_ABSTRACTION = "Account Abstraction Transaction",
+}
