bd0b0a99b4031cc39413eb0012d516c1273ca239
User <user@MacBook-Pro-de-User.local>
2024-01-18 14:53:29 +0100

    dd

--- webPage/src/App.jsx
+++ webPage/src/App.jsx
@@ -6,9 +6,20 @@ function App() {
   return (
     <div>
       <GoldRushProvider apikey={"cqt_rQf6TP9W6WKQGWJvm7rcVKKRCj9v"}>
-        <BlockDetailsDisplay chain_name={'eth-mainnet'}
+        {/* <BlockDetailsDisplay chain_name={'eth-mainnet'}
     block_id={'12345678'}
-    icon_url={"https://goldrush.vercel.app/icons/token.svg"}/>
+    icon_url={"https://goldrush.vercel.app/icons/token.svg"}/> */}
+      <TokenBalancesListView
+              chain_names={[
+                  "eth-mainnet",
+                  "matic-mainnet",
+                  "bsc-mainnet",
+                  "avalanche-mainnet",
+                  "optimism-mainnet",
+              ]}
+              hide_small_balances
+              address="0xfc43f5f9dd45258b3aff31bdbe6561d97e8b71de"
+          />
       </GoldRushProvider>
     </div>
   )
