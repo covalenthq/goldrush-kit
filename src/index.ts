895291948acb9dd4905e31524b245b0d767204cc
nguyenlejoe <joe_le.1@hotmail.com>
2024-01-23 11:41:32 -0800

    XYK Organism release 0.3.4

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,16 @@ export { TokenTransfersListView } from "./components/Organisms/TokenBalances/Tok
 export { NFTWalletTokenListView } from "./components/Organisms/NFTs/NFTWalletTokenListView/NFTWalletTokenListView";
 export { NFTCollectionTokenListView } from "./components/Organisms/NFTs/NFTCollectionTokenListView/NFTCollectionTokenListView";
 export { NFTDetailView } from "./components/Organisms/NFTs/NFTDetailView/NFTDetailView";
+
+export { XYKPoolDetailView } from "./components/Organisms/XYK/XYKPoolDetailView/XYKPoolDetailView";
+export { XYKPoolListView } from "./components/Organisms/XYK/XYKPoolListView/XYKPoolListView";
+export { XYKPoolTransactionsListView } from "./components/Organisms/XYK/XYKPoolTransactionsListView/XYKPoolTransactionsListView";
+export { XYKTokenListView } from "./components/Organisms/XYK/XYKTokenListView/XYKTokenListView";
+export { XYKTokenPoolListView } from "./components/Organisms/XYK/XYKTokenPoolListView/XYKTokenPoolListView";
+export { XYKTokenTransactionsListView } from "./components/Organisms/XYK/XYKTokenTransactionsListView/XYKTokenTransactionsListView";
+export { XYKWalletPoolListView } from "./components/Organisms/XYK/XYKWalletPoolListView/XYKWalletPoolListView";
+export { XYKWalletTransactionsListView } from "./components/Organisms/XYK/XYKWalletTransactionsListView/XYKWalletTransactionsListView";
+
 export { type NFTDetailViewProps } from "./utils/types/organisms.types";
 export { type AddressActivityListViewProps } from "./utils/types/organisms.types";
 export { type TokenBalancesListViewProps } from "./utils/types/organisms.types";
