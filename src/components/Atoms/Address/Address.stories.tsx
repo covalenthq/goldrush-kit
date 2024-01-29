6924f9df92e7b9ce3c1234dace697e853c726395
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2024-01-29 21:28:58 +0530

    refactor(atoms): consistent stories

--- src/components/Atoms/Address/Address.stories.tsx
+++ src/components/Atoms/Address/Address.stories.tsx
@@ -1,16 +1,16 @@
 import { type Meta, type StoryObj } from "@storybook/react";
-import { Address } from "./Address";
+import { Address as AddressComponent } from "./Address";
 
-const meta: Meta<typeof Address> = {
+const meta: Meta<typeof AddressComponent> = {
     title: "Atoms/Address",
-    component: Address,
+    component: AddressComponent,
 };
 
 export default meta;
 
-type Story = StoryObj<typeof Address>;
+type Story = StoryObj<typeof AddressComponent>;
 
-export const AddressDisplay: Story = {
+export const Address: Story = {
     args: {
         address: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
     },
