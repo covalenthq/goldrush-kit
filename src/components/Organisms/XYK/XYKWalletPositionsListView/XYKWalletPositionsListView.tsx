bc603680a71a0c8d9de6299747a028699e6eed84
nguyenlejoe <joe_le.1@hotmail.com>
2024-01-31 11:27:46 -0800

    Add wallet position for XYK

---
+++ src/components/Organisms/XYK/XYKWalletPositionsListView/XYKWalletPositionsListView.tsx
@@ -0,0 +1,337 @@
+import { type Option, None, Some } from "@/utils/option";
+import {
+    type UniswapLikeBalanceItem,
+    prettifyCurrency,
+    calculatePrettyBalance,
+} from "@covalenthq/client-sdk";
+import { useEffect, useState } from "react";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+    type ColumnDef,
+    type SortingState,
+    flexRender,
+    getCoreRowModel,
+    getSortedRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import { Checkbox } from "@/components/ui/checkbox";
+import { TokenAvatar } from "../../../Atoms/TokenAvatar/TokenAvatar";
+import { Button } from "@/components/ui/button";
+import { TableHeaderSorting } from "@/components/ui/tableHeaderSorting";
+import { IconWrapper } from "@/components/Shared";
+import { GRK_SIZES } from "@/utils/constants/shared.constants";
+import { useCovalent } from "@/utils/store/Covalent";
+import { type XYKWalletPositionsListViewProps } from "@/utils/types/organisms.types";
+import { SkeletonTable } from "@/components/ui/skeletonTable";
+
+export const XYKWalletPositionsListView: React.FC<
+    XYKWalletPositionsListViewProps
+> = ({ chain_name, dex_name, on_pool_click, wallet_address }) => {
+    const { covalentClient } = useCovalent();
+
+    const [sorting, setSorting] = useState<SortingState>([
+        {
+            id: "total_liquidity_quote",
+            desc: true,
+        },
+    ]);
+    const [rowSelection, setRowSelection] = useState({});
+    const [maybeResult, setResult] =
+        useState<Option<UniswapLikeBalanceItem[]>>(None);
+    const [error, setError] = useState({ error: false, error_message: "" });
+
+    useEffect(() => {
+        (async () => {
+            setResult(None);
+            let response;
+            try {
+                response =
+                    await covalentClient.XykService.getAddressExchangeBalances(
+                        chain_name,
+                        dex_name,
+                        wallet_address
+                    );
+                setError({ error: false, error_message: "" });
+                setResult(new Some(response.data.items));
+            } catch (exception) {
+                setResult(new Some([]));
+                setError({
+                    error: response ? response.error : false,
+                    error_message: response ? response.error_message : "",
+                });
+            }
+            console.log(response);
+        })();
+    }, [chain_name, dex_name, wallet_address]);
+
+    const columns: ColumnDef<UniswapLikeBalanceItem>[] = [
+        {
+            id: "select",
+            header: ({ table }) => (
+                <Checkbox
+                    className="mx-1"
+                    checked={table.getIsAllPageRowsSelected()}
+                    onCheckedChange={(value) =>
+                        table.toggleAllPageRowsSelected(!!value)
+                    }
+                    aria-label="Select all"
+                />
+            ),
+            cell: ({ row }) => (
+                <Checkbox
+                    className="mx-1"
+                    checked={row.getIsSelected()}
+                    onCheckedChange={(value) => row.toggleSelected(!!value)}
+                    aria-label="Select row"
+                />
+            ),
+            enableSorting: false,
+            enableHiding: false,
+        },
+        {
+            id: "contract_name",
+            accessorKey: "contract_name",
+            header: ({ column }) => (
+                <TableHeaderSorting
+                    align="left"
+                    header_name={"Pool"}
+                    column={column}
+                />
+            ),
+            cell: ({ row }) => {
+                const token_0 = row.original.token_0;
+                const token_1 = row.original.token_1;
+                const pool = `${token_0.contract_ticker_symbol}-${token_1.contract_ticker_symbol}`;
+
+                return (
+                    <div className="flex items-center gap-3">
+                        <div className="relative mr-2 flex">
+                            <TokenAvatar
+                                size={GRK_SIZES.EXTRA_SMALL}
+                                token_url={token_0.logo_url}
+                            />
+                            <div className="absolute left-4">
+                                <TokenAvatar
+                                    size={GRK_SIZES.EXTRA_SMALL}
+                                    token_url={token_1.logo_url}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col">
+                            <label className="text-base">
+                                {pool ? pool : "FIXME"}
+                            </label>
+                        </div>
+                    </div>
+                );
+            },
+        },
+        {
+            id: "total_liquidity_quote",
+            accessorKey: "total_liquidity_quote",
+            header: ({ column }) => (
+                <TableHeaderSorting
+                    align="right"
+                    header_name={"Liquidity"}
+                    column={column}
+                />
+            ),
+            cell: ({ row }) => {
+                const valueFormatted = prettifyCurrency(
+                    row.original.token_0.quote + row.original.token_1.quote
+                );
+                return <div className="text-right">{valueFormatted}</div>;
+            },
+        },
+        {
+            id: "token_0",
+            accessorKey: "token_0",
+            header: ({ column }) => (
+                <TableHeaderSorting
+                    align="right"
+                    header_name={"Token amount"}
+                    column={column}
+                />
+            ),
+            cell: ({ row }) => {
+                const token = row.original.token_0;
+                const valueFormatted = token.balance
+                    ? calculatePrettyBalance(
+                          token.balance,
+                          token.contract_decimals,
+                          true,
+                          4
+                      )
+                    : 0;
+
+                return (
+                    <div className="text-right">
+                        {`${valueFormatted} ${token.contract_ticker_symbol}`}
+                    </div>
+                );
+            },
+        },
+        {
+            id: "token_1",
+            accessorKey: "token_1",
+            header: ({ column }) => (
+                <TableHeaderSorting
+                    align="right"
+                    header_name={"Token amount"}
+                    column={column}
+                />
+            ),
+            cell: ({ row }) => {
+                const token = row.original.token_1;
+                const valueFormatted = token.balance
+                    ? calculatePrettyBalance(
+                          token.balance,
+                          token.contract_decimals,
+                          true,
+                          4
+                      )
+                    : 0;
+
+                return (
+                    <div className="text-right">
+                        {`${valueFormatted} ${token.contract_ticker_symbol}`}
+                    </div>
+                );
+            },
+        },
+        {
+            id: "actions",
+            cell: ({ row }) => {
+                return (
+                    <div className="text-right">
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="ghost" className="ml-auto  ">
+                                    <span className="sr-only">Open menu</span>
+                                    <IconWrapper icon_class_name="expand_more" />
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                <DropdownMenuItem
+                                    onClick={() => {
+                                        if (on_pool_click) {
+                                            on_pool_click(
+                                                row.original.pool_token
+                                                    .contract_address
+                                            );
+                                        }
+                                    }}
+                                >
+                                    <IconWrapper
+                                        icon_class_name="swap_horiz"
+                                        class_name="mr-2"
+                                    />{" "}
+                                    View Pool
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
+                );
+            },
+        },
+    ];
+
+    const table = useReactTable({
+        data: maybeResult.match({
+            None: () => [],
+            Some: (result) => result,
+        }),
+        columns: columns,
+        onSortingChange: setSorting,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        onRowSelectionChange: setRowSelection,
+        state: {
+            sorting,
+            rowSelection,
+        },
+    });
+
+    const body = maybeResult.match({
+        None: () => <SkeletonTable cols={5} float="right" />,
+        Some: () =>
+            error.error ? (
+                <TableRow>
+                    <TableCell
+                        colSpan={columns.length}
+                        className="h-24 text-center"
+                    >
+                        {error.error_message}
+                    </TableCell>
+                </TableRow>
+            ) : !error.error && table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                    <TableRow
+                        key={row.id}
+                        data-state={row.getIsSelected() && "selected"}
+                    >
+                        {row.getVisibleCells().map((cell) => (
+                            <TableCell key={cell.id}>
+                                {flexRender(
+                                    cell.column.columnDef.cell,
+                                    cell.getContext()
+                                )}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                ))
+            ) : (
+                <TableRow>
+                    <TableCell
+                        colSpan={columns.length}
+                        className="h-24 text-center"
+                    >
+                        No results.
+                    </TableCell>
+                </TableRow>
+            ),
+    });
+
+    return (
+        <div className="space-y-4">
+            <Table>
+                <TableHeader>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => {
+                                return (
+                                    <TableHead key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                  header.column.columnDef
+                                                      .header,
+                                                  header.getContext()
+                                              )}
+                                    </TableHead>
+                                );
+                            })}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+                <TableBody>{body}</TableBody>
+            </Table>
+        </div>
+    );
+};
