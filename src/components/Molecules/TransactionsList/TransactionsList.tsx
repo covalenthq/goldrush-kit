eb50441a9c44d04426585e86e31c025c00c8b8c0
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2024-01-31 19:55:55 +0530

    fix(molecules/tx-list): deps

--- src/components/Molecules/TransactionsList/TransactionsList.tsx
+++ src/components/Molecules/TransactionsList/TransactionsList.tsx
@@ -83,16 +83,18 @@ export const TransactionsList: React.FC<TransactionListProps> = ({
             });
             const results = await Promise.all(promises);
             setResult(new Some(results.flat()));
-
-            maybeResult.match({
-                None: () => [],
-                Some: (result) => {
-                    setFilterResult(new Some(result));
-                    return [];
-                },
-            });
         })();
     }, [chain_names, address]);
+
+    useEffect(() => {
+        maybeResult.match({
+            None: () => [],
+            Some: (result) => {
+                setFilterResult(new Some(result));
+                return [];
+            },
+        });
+    }, [maybeResult]);
 
     const columns: ColumnDef<CrossChainTransaction>[] = [
         {
@@ -270,181 +272,12 @@ export const TransactionsList: React.FC<TransactionListProps> = ({
         },
     ];
 
-    // const mobile_columns: ColumnDef<CrossChainTransaction>[] = [
-    //     {
-    //         id: "select",
-    //         header: ({ table }) => (
-    //             <Checkbox
-    //                 checked={table.getIsAllPageRowsSelected()}
-    //                 onCheckedChange={(value) =>
-    //                     table.toggleAllPageRowsSelected(!!value)
-    //                 }
-    //                 aria-label="Select all"
-    //             />
-    //         ),
-    //         cell: ({ row }) => (
-    //             <Checkbox
-    //                 checked={row.getIsSelected()}
-    //                 onCheckedChange={(value) => row.toggleSelected(!!value)}
-    //                 aria-label="Select row"
-    //             />
-    //         ),
-    //         enableSorting: false,
-    //         enableHiding: false,
-    //     },
-    //     {
-    //         id: "contract_name",
-    //         accessorKey: "contract_name",
-    //         // header: <div className="text-left">Token</div>,
-    //         header: ({ column }) => (
-    //             <TableHeaderSorting
-    //                 align="left"
-    //                 header_name={"Token"}
-    //                 column={column}
-    //             />
-    //         ),
-    //         cell: ({ row }) => {
-    //             const original = row.original as BalanceItem;
-    //             const valueFormatted = calculatePrettyBalance(
-    //                 original.balance ?? 0,
-    //                 row.original.contract_decimals,
-    //                 true,
-    //                 4
-    //             );
-    //             const formattedNumber = parseFloat(
-    //                 valueFormatted.toString()
-    //             ).toLocaleString(undefined, {
-    //                 minimumFractionDigits: 4,
-    //                 maximumFractionDigits: 4,
-    //             });
-
-    //             const chain: ChainItem | null =
-    //                 chains?.find((o) => o.name === row.original.chain) ?? null;
-    //             const chainColor = chain?.color_theme.hex;
-    //             const chain_label = (
-    //                 chain?.label ? chain.label : "FIXME"
-    //             ).replace(" Mainnet", "");
-    //             const protocol_url = row.original.logo_urls.protocol_logo_url;
-
-    //             return (
-    //                 <div className="flex items-center gap-3">
-    //                     <TokenAvatar
-    //                         size={GRK_SIZES.EXTRA_SMALL}
-    //                         chain_color={chainColor}
-    //                         sub_url={protocol_url}
-    //                         token_url={row.original.logo_urls.token_logo_url}
-    //                     />
-    //                     <div className="flex flex-col gap-1">
-    //                         <div style={{ color: chainColor }}>
-    //                             {chain_label}
-    //                         </div>
-    //                         <label className="text-base">
-    //                             {row.getValue("contract_display_name")}
-    //                         </label>
-    //                         <div className="text-secondary">
-    //                             {!mask_balances ? formattedNumber : "*****"}{" "}
-    //                             {row.original.contract_ticker_symbol}
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             );
-    //         },
-    //     },
-    //     {
-    //         id: "pretty_quote",
-    //         accessorKey: "quote",
-    //         header: ({ column }) => (
-    //             <TableHeaderSorting
-    //                 align="right"
-    //                 header_name={"Quote"}
-    //                 column={column}
-    //             />
-    //         ),
-    //         cell: ({ row }) => {
-    //             return (
-    //                 <div className="flex flex-col">
-    //                     <div className="text-right text-base">
-    //                         {row.original.pretty_quote
-    //                             ? row.original.pretty_quote
-    //                             : "$0.00"}
-    //                     </div>
-    //                     <div className="text-right ">
-    //                         <BalancePriceDelta
-    //                             numerator={row.original.quote_24h}
-    //                             denominator={row.original.quote}
-    //                         />
-    //                     </div>
-    //                 </div>
-    //             );
-    //         },
-    //     },
-    //     {
-    //         id: "actions",
-    //         cell: ({ row }) => {
-    //             return (
-    //                 <div className="text-right">
-    //                     <DropdownMenu>
-    //                         <DropdownMenuTrigger asChild>
-    //                             <Button variant="ghost" className="ml-auto  ">
-    //                                 <span className="sr-only">Open menu</span>
-    //                                 <IconWrapper icon_class_name="expand_more" />
-    //                             </Button>
-    //                         </DropdownMenuTrigger>
-    //                         <DropdownMenuContent align="end">
-    //                             <DropdownMenuLabel>Actions</DropdownMenuLabel>
-    //                             <DropdownMenuItem
-    //                                 onClick={() => {
-    //                                     if (on_transfer_click) {
-    //                                         on_transfer_click(
-    //                                             row.original.contract_address
-    //                                         );
-    //                                     }
-    //                                 }}
-    //                             >
-    //                                 <IconWrapper
-    //                                     icon_class_name="swap_horiz"
-    //                                     class_name="mr-2"
-    //                                 />{" "}
-    //                                 View Transfer History
-    //                             </DropdownMenuItem>
-    //                             {/* <DropdownMenuItem>
-    //                                 <IconWrapper
-    //                                     icon_class_name="swap_calls"
-    //                                     class_name="mr-2"
-    //                                 />{" "}
-    //                                 Swap {row.original.contract_ticker_symbol}
-    //                             </DropdownMenuItem> */}
-    //                             <DropdownMenuSeparator />
-    //                             <DropdownMenuLabel>
-    //                                 <div className="flex">
-    //                                     <IconWrapper
-    //                                         icon_class_name="history"
-    //                                         class_name="mr-2"
-    //                                     />
-    //                                     {row.original.last_transferred_at
-    //                                         ? `Last transfered ${timestampParser(
-    //                                               row.original.last_transferred_at.toDateString(),
-    //                                               "relative"
-    //                                           )} `
-    //                                         : "FIX ME"}
-    //                                 </div>{" "}
-    //                             </DropdownMenuLabel>
-    //                         </DropdownMenuContent>
-    //                     </DropdownMenu>
-    //                 </div>
-    //             );
-    //         },
-    //     },
-    // ];
-
     const table = useReactTable({
         data: filterResult.match({
             None: () => [],
             Some: (result) => result,
         }),
-        columns:
-            // windowWidth < 700 ? mobile_columns :
-            columns,
+        columns: columns,
         onSortingChange: setSorting,
         getCoreRowModel: getCoreRowModel(),
         getSortedRowModel: getSortedRowModel(),
