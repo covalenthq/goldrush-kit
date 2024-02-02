72b3377f33723188541645ac65ee421c1a3bcb74
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2024-02-02 15:40:59 +0530

    feat(org/tx-rec): ui

--- src/components/Organisms/TransactionReceiptView/TransactionReceiptView.tsx
+++ src/components/Organisms/TransactionReceiptView/TransactionReceiptView.tsx
@@ -1,5 +1,5 @@
 import { type TransactionReceiptViewProps } from "@/utils/types/organisms.types";
-import { TypographyH1 } from "@/components/ui/typography";
+import { TypographyH4 } from "@/components/ui/typography";
 import { Address } from "@/components/Atoms/Address/Address";
 import { DecodedTransaction } from "@/components/Molecules/DecodedTransaction/DecodedTransaction";
 import { useMemo, useState } from "react";
@@ -7,10 +7,9 @@ import { type Option, None } from "@/utils/option";
 import { type DecodedTransactionMetadata } from "@/utils/types/molecules.types";
 import { timestampParser } from "@/utils/functions";
 import { AccountCardView } from "@/components/Molecules/AccountCardView/AccountCardView";
-import { CardContent, CardDescription } from "@/components/ui/card";
+import { CardDescription } from "@/components/ui/card";
 import { type ChainItem, calculatePrettyBalance } from "@covalenthq/client-sdk";
 import { useCovalent } from "@/utils/store/Covalent";
-import { Button } from "@/components/ui/button";
 
 export const TransactionReceiptView: React.FC<TransactionReceiptViewProps> = ({
     chain_name,
@@ -26,139 +25,150 @@ export const TransactionReceiptView: React.FC<TransactionReceiptViewProps> = ({
     }, [chains, chain_name]);
 
     return (
-        <section className="flex w-full flex-col gap-y-8">
-            <div className="flex gap-x-8">
-                <TypographyH1>Transaction Receipt</TypographyH1>
-            </div>
-
-            {maybeResult.match({
-                None: () => null,
-                Some: (metadata) => (
-                    <>
-                        <div className="flex items-center gap-x-8">
-                            <div>
-                                <CardDescription>
-                                    Transaction Hash
-                                </CardDescription>
-                                <CardContent>
-                                    <Address address={tx_hash} />
-                                </CardContent>
-                            </div>
-
-                            <div>
-                                <CardDescription>Network</CardDescription>
-                                <CardContent>{CHAIN?.label}</CardContent>
-                            </div>
-
-                            <div>
-                                <CardDescription>
-                                    Transaction Time
-                                </CardDescription>
-                                <CardContent>
-                                    {timestampParser(
-                                        metadata.block_signed_at.toString(),
-                                        "descriptive"
-                                    )}
-                                </CardContent>
-                            </div>
-
-                            <div>
-                                <a
-                                    href={metadata.explorers[0].url}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    <Button role="link">
-                                        View on{" "}
-                                        {metadata.explorers[0].label ??
-                                            "Explorer"}
-                                    </Button>
-                                </a>
-                            </div>
-                        </div>
-
-                        <div className="flex gap-x-8">
-                            <div className="flex flex-col">
-                                <CardDescription>Address</CardDescription>
-                                <AccountCardView
-                                    address={metadata.from_address}
-                                    name={metadata.from_address_label}
-                                />
-                            </div>
-
-                            <div className="flex flex-col">
-                                <CardDescription>
-                                    Interacted with
-                                </CardDescription>
-                                <AccountCardView
-                                    address={metadata.to_address}
-                                    name={metadata.to_address_label}
-                                />
-                            </div>
-                        </div>
-                    </>
-                ),
-            })}
-
-            <DecodedTransaction
-                chain_name={chain_name}
-                tx_hash={tx_hash}
-                setMetadata={setResult}
+        <section className="h-fit w-5/6 max-w-[37.5rem] overflow-hidden rounded border">
+            <figure
+                className="h-14 min-h-14 w-full"
+                style={{
+                    backgroundImage: `linear-gradient(to right, ${CHAIN?.color_theme.hex}, ${CHAIN?.color_theme.hex})`,
+                }}
             />
 
-            {maybeResult.match({
-                None: () => null,
-                Some: (metadata) => (
-                    <>
-                        <div className="flex gap-x-8">
-                            <div>
+            <main className="flex h-full w-full flex-col gap-y-4 p-4">
+                <header className="flex flex-col gap-y-1">
+                    <TypographyH4>Transaction Receipt</TypographyH4>
+                    <CardDescription>
+                        <Address address={tx_hash} />
+                    </CardDescription>
+                </header>
+
+                {maybeResult.match({
+                    None: () => null,
+                    Some: (metadata) => (
+                        <>
+                            <div className="flex flex-col gap-y-2">
                                 <CardDescription>
-                                    Transaction Fee
+                                    Network:{" "}
+                                    <span className="text-black">
+                                        {CHAIN?.label}
+                                    </span>
                                 </CardDescription>
-                                <CardContent className="flex items-center gap-x-2">
-                                    {calculatePrettyBalance(
-                                        BigInt(metadata.fees_paid || 0)!,
-                                        metadata.gas_metadata.contract_decimals
-                                    )}{" "}
-                                    {
-                                        metadata.gas_metadata
-                                            .contract_ticker_symbol
-                                    }{" "}
+
+                                <CardDescription>
+                                    Transaction Time:{" "}
+                                    <span className="text-black">
+                                        {timestampParser(
+                                            metadata.block_signed_at.toString(),
+                                            "descriptive"
+                                        )}
+                                    </span>
+                                </CardDescription>
+                            </div>
+
+                            <div className="flex gap-x-8">
+                                <div className="flex flex-col">
+                                    <CardDescription>Address</CardDescription>
+                                    <AccountCardView
+                                        address={metadata.from_address}
+                                        name={metadata.from_address_label}
+                                    />
+                                </div>
+
+                                <div className="flex flex-col">
                                     <CardDescription>
-                                        {" "}
+                                        Interacted with
+                                    </CardDescription>
+                                    <AccountCardView
+                                        address={metadata.to_address}
+                                        name={metadata.to_address_label}
+                                    />
+                                </div>
+                            </div>
+                        </>
+                    ),
+                })}
+
+                <DecodedTransaction
+                    chain_name={chain_name}
+                    tx_hash={tx_hash}
+                    setMetadata={setResult}
+                />
+
+                {maybeResult.match({
+                    None: () => null,
+                    Some: (metadata) => (
+                        <>
+                            <div className="flex flex-col gap-y-2">
+                                <div>
+                                    <CardDescription>
+                                        Transaction Fee
+                                    </CardDescription>
+                                    <CardDescription>
+                                        <span className="text-black">
+                                            {calculatePrettyBalance(
+                                                BigInt(
+                                                    metadata.fees_paid || 0
+                                                )!,
+                                                metadata.gas_metadata
+                                                    .contract_decimals
+                                            )}{" "}
+                                            {
+                                                metadata.gas_metadata
+                                                    .contract_ticker_symbol
+                                            }{" "}
+                                        </span>
                                         {metadata.pretty_gas_quote}
                                     </CardDescription>
-                                </CardContent>
+                                </div>
+
+                                <div>
+                                    <CardDescription>
+                                        Exchange Rate
+                                    </CardDescription>
+                                    <CardDescription>
+                                        <span className="text-black">
+                                            1 USD ={" "}
+                                            {1 / (metadata.gas_quote_rate ?? 1)}{" "}
+                                            {
+                                                metadata.gas_metadata
+                                                    .contract_ticker_symbol
+                                            }
+                                        </span>
+                                    </CardDescription>
+                                </div>
+
+                                <div>
+                                    <CardDescription>Gas Price</CardDescription>
+                                    <CardDescription>
+                                        <span className="text-black">
+                                            {calculatePrettyBalance(
+                                                BigInt(metadata.gas_price),
+                                                metadata.gas_metadata
+                                                    .contract_decimals,
+                                                true,
+                                                10
+                                            )}{" "}
+                                            {
+                                                metadata.gas_metadata
+                                                    .contract_ticker_symbol
+                                            }
+                                        </span>
+                                    </CardDescription>
+                                </div>
                             </div>
 
-                            <div>
-                                <CardDescription>Exchange Rate</CardDescription>
-                                <CardContent>
-                                    1 USD = {1 / (metadata.gas_quote_rate ?? 1)}{" "}
-                                    {
-                                        metadata.gas_metadata
-                                            .contract_ticker_symbol
-                                    }
-                                </CardContent>
-                            </div>
-
-                            <div>
-                                <CardDescription>Gas Price</CardDescription>
-                                <CardContent>
-                                    {calculatePrettyBalance(
-                                        BigInt(metadata.gas_price),
-                                        metadata.gas_metadata.contract_decimals
-                                    )}{" "}
-                                    {
-                                        metadata.gas_metadata
-                                            .contract_ticker_symbol
-                                    }
-                                </CardContent>
-                            </div>
-                        </div>
-                    </>
-                ),
-            })}
+                            <a
+                                href={metadata.explorers[0].url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-sm font-semibold hover:underline"
+                            >
+                                View on{" "}
+                                {metadata.explorers[0].label ?? "Explorer"}
+                            </a>
+                        </>
+                    ),
+                })}
+            </main>
         </section>
     );
 };
