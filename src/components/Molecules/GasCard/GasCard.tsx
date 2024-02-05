b0060ac99dc6a529236f64b72ef39d88157236bd
nguyenlejoe <joe_le.1@hotmail.com>
2024-02-05 13:05:17 -0800

    error state

--- src/components/Molecules/GasCard/GasCard.tsx
+++ src/components/Molecules/GasCard/GasCard.tsx
@@ -4,6 +4,8 @@ import { useCovalent } from "@/utils/store/Covalent";
 import type { Option } from "@/utils/option";
 import { None, Some } from "@/utils/option";
 import type { GasPricesResponse, PriceItem } from "@covalenthq/client-sdk";
+import { Skeleton } from "@/components/ui/skeleton";
+import { GRK_SIZES } from "@/utils/constants/shared.constants";
 
 export const GasCard: React.FC<GasCardProps> = ({ chain_name, event_type }) => {
     const [maybeResult, setResult] = useState<Option<GasPricesResponse>>(None);
@@ -23,6 +25,7 @@ export const GasCard: React.FC<GasCardProps> = ({ chain_name, event_type }) => {
                     error: response ? response.error : false,
                     error_message: response ? response.error_message : "",
                 });
+                setResult(new Some(response.data));
                 return;
             }
             setResult(new Some(response.data));
@@ -52,71 +55,88 @@ export const GasCard: React.FC<GasCardProps> = ({ chain_name, event_type }) => {
                     </label>
                 </div>
                 <div className="flex w-full flex-col gap-6 p-6">
-                    {error && (
-                        <div className="flex items-center justify-center">
-                            {error.error_message}
-                        </div>
-                    )}
-                    {!error &&
-                        maybeResult.match({
-                            None: () => <>Loading</>,
-                            Some: (result: any) => {
-                                console.log(result);
-                                return result.items
-                                    .sort(
-                                        (
-                                            a: { gas_price: string },
-                                            b: { gas_price: string }
-                                        ) =>
-                                            parseInt(a.gas_price) -
-                                            parseInt(b.gas_price)
-                                    )
-                                    .map((o: PriceItem, i: number) => {
-                                        const gwei = Math.round(
-                                            parseInt(o.gas_price) /
-                                                Math.pow(10, 9)
-                                        );
+                    {maybeResult.match({
+                        None: () => {
+                            return (
+                                <div className="flex flex-col gap-3">
+                                    {[1, 2, 3].map((o, i) => {
                                         return (
                                             <div
-                                                className="flex justify-between "
+                                                className="flex justify-between"
                                                 key={i}
                                             >
-                                                <div className="flex items-center gap-4">
-                                                    <div className="text-3xl">
-                                                        {i === 0
-                                                            ? "🚴"
-                                                            : i === 1
-                                                              ? "🚗"
-                                                              : "🚄"}
-                                                    </div>
-                                                    <div className="flex flex-col">
-                                                        <label>
-                                                            {i === 0
-                                                                ? "Low"
-                                                                : i === 1
-                                                                  ? "Normal"
-                                                                  : "High"}
-                                                        </label>
-                                                        <label className="text-sm text-secondary">
-                                                            {o.interval}
-                                                        </label>
-                                                    </div>
+                                                <Skeleton
+                                                    size={GRK_SIZES.LARGE}
+                                                />
+                                                <Skeleton
+                                                    size={GRK_SIZES.LARGE}
+                                                />
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            );
+                        },
+                        Some: (result: any) => {
+                            if (error.error) {
+                                return (
+                                    <div className="flex items-center justify-center">
+                                        {error.error_message}
+                                    </div>
+                                );
+                            }
+                            return result.items
+                                .sort(
+                                    (
+                                        a: { gas_price: string },
+                                        b: { gas_price: string }
+                                    ) =>
+                                        parseInt(a.gas_price) -
+                                        parseInt(b.gas_price)
+                                )
+                                .map((o: PriceItem, i: number) => {
+                                    const gwei = Math.round(
+                                        parseInt(o.gas_price) / Math.pow(10, 9)
+                                    );
+                                    return (
+                                        <div
+                                            className="flex justify-between "
+                                            key={i}
+                                        >
+                                            <div className="flex items-center gap-4">
+                                                <div className="text-3xl">
+                                                    {i === 0
+                                                        ? "🚴"
+                                                        : i === 1
+                                                          ? "🚗"
+                                                          : "🚄"}
                                                 </div>
                                                 <div className="flex flex-col">
                                                     <label>
-                                                        {gwei.toFixed(0)} Gwei
+                                                        {i === 0
+                                                            ? "Low"
+                                                            : i === 1
+                                                              ? "Normal"
+                                                              : "High"}
                                                     </label>
                                                     <label className="text-sm text-secondary">
-                                                        {
-                                                            o.pretty_total_gas_quote
-                                                        }
+                                                        {o.interval}
                                                     </label>
                                                 </div>
                                             </div>
-                                        );
-                                    });
-                            },
-                        })}
+                                            <div className="flex flex-col">
+                                                <label>
+                                                    {gwei.toFixed(0)} Gwei
+                                                </label>
+                                                <label className="text-sm text-secondary">
+                                                    {o.pretty_total_gas_quote}
+                                                </label>
+                                            </div>
+                                        </div>
+                                    );
+                                });
+                        },
+                    })}
                 </div>
             </div>
         </>
