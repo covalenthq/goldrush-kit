af635a07583601e234d6a7e99c999c6161cf3a15
Dennis Zhang <zhang_dennis1999@hotmail.com>
2024-02-17 15:35:30 -0800

    swap

--- src/utils/functions/exchange-type.ts
+++ src/utils/functions/exchange-type.ts
@@ -1,19 +1,19 @@
 export const handleExchangeType = (transaction: any, token_num: number) => {
     if (token_num === 0) {
         if (transaction.amount_0_in === "0") {
-            return "in";
+            return "out";
         }
         if (transaction.amount_0_out === "0") {
-            return "out";
+            return "in";
         }
     }
     if (token_num === 1) {
         if (transaction.amount_1_in === "0") {
-            return "in";
+            return "out";
         }
 
         if (transaction.amount_1_out === "0") {
-            return "out";
+            return "in";
         }
     }
 };
