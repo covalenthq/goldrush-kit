4c48a47a81701b087bc42a071803d8f25322198b
Karan Pargal <karanpargal007@gmail.com>
2024-03-23 12:43:01 +0530

    fix: update main to ESM

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,25 @@
-module.exports = {
-    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-    staticDirs: ["../src/static/"],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/addon-interactions",
-        "storybook-addon-themes",
-        "storybook-dark-mode",
-        "@storybook/addon-designs",
-        // 'storybook-tailwind-dark-mode',
-    ],
-    typescript: {
-        reactDocgen: "react-docgen-typescript",
-    },
-    framework: {
-        name: "@storybook/react-vite",
-        options: {},
-    },
-    docs: {
-        autodocs: "tag",
-    },
-};
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../src/static/"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "storybook-addon-themes",
+    "storybook-dark-mode",
+    "@storybook/addon-designs",
+    // 'storybook-tailwind-dark-mode',
+  ],
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+});
