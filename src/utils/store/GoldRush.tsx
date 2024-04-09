1d5c6131a54062a15f9026fba8bb541ab8c34d6d
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2024-04-09 17:25:49 +0530

    fix(theme): state updation

--- src/utils/store/GoldRush.tsx
+++ src/utils/store/GoldRush.tsx
@@ -10,11 +10,11 @@ import {
     type GoldRushThemeType,
     type GoldRushContextType,
     type GoldRushProviderProps,
-    type GoldRushThemeColorType,
 } from "../types/store.types";
 import { type ChainItem, CovalentClient } from "@covalenthq/client-sdk";
 import { primaryShades } from "../functions";
 import { SEARCH_RESULTS_TYPE } from "../constants/shared.constants";
+import defaultsDeep from "lodash/defaultsDeep";
 
 const GoldRushContext = createContext<GoldRushContextType>(
     {} as GoldRushContextType
@@ -55,7 +55,12 @@ export const GoldRushProvider: React.FC<GoldRushProviderProps> = ({
 
     const [chains, setChains] = useState<ChainItem[] | null>(null);
     const [selectedChain, setSelectedChain] = useState<ChainItem | null>(null);
-    const [theme, setTheme] = useState<GoldRushThemeType>(defaultTheme);
+    const [theme, setTheme] = useState<GoldRushThemeType>(
+        defaultsDeep(
+            JSON.parse(localStorage.getItem("goldrush_theme") || "null") ?? {},
+            defaultsDeep(newTheme, defaultTheme)
+        )
+    );
 
     useEffect(() => {
         (async () => {
@@ -72,113 +77,69 @@ export const GoldRushProvider: React.FC<GoldRushProviderProps> = ({
     }, []);
 
     useEffect(() => {
-        const existingTheme = localStorage.getItem("goldrush_theme") || null;
+        localStorage.setItem("goldrush_theme", JSON.stringify(theme));
+        const { borderRadius, colors, mode, style } = theme;
 
-        if (!existingTheme) {
-            applyThemeHandler(defaultTheme);
-        } else {
-            applyThemeHandler(JSON.parse(existingTheme));
+        const body = document.body;
+        const root = document.documentElement;
+
+        switch (mode) {
+            case "dark": {
+                body.classList.add("dark");
+                root.classList.add("dark");
+                break;
+            }
+            case "light": {
+                body.classList.remove("dark");
+                root.classList.remove("dark");
+                break;
+            }
         }
 
-        if (newTheme) {
-            updateThemeHandler(newTheme);
+        switch (style) {
+            case "neo": {
+                body.classList.add("neo");
+                root.classList.add("neo");
+                break;
+            }
+            case "classic": {
+                body.classList.remove("neo");
+                root.classList.remove("neo");
+                break;
+            }
         }
-    }, []);
+
+        root.style.setProperty("--grk-border-radius", `${borderRadius}px`);
+
+        Object.entries(colors).forEach(([_mode, _types]) => {
+            Object.entries(_types).forEach(([_type, value]) => {
+                if (_type === "primary") {
+                    const shades = primaryShades(
+                        value,
+                        _mode as GoldRushThemeType["mode"]
+                    );
+                    Object.entries(shades).forEach(([shade, color]) => {
+                        root.style.setProperty(
+                            `--grk-${_type}-${_mode}-${shade}`,
+                            color
+                        );
+                    });
+                } else {
+                    root.style.setProperty(`--grk-${_type}-${_mode}`, value);
+                }
+            });
+        });
+    }, [theme]);
 
     const updateThemeHandler = useCallback(
-        ({ borderRadius, colors, mode, style }: Partial<GoldRushThemeType>) => {
-            const updatedTheme: GoldRushThemeType = { ...theme };
-
-            if (borderRadius) {
-                updatedTheme.borderRadius = borderRadius;
-            }
-            if (mode) {
-                updatedTheme.mode = mode;
-            }
-            if (style) {
-                updatedTheme.style = style;
-            }
-            if (colors) {
-                Object.entries(colors).forEach(([_mode, _types]) => {
-                    Object.entries(_types).forEach(([_type, value]) => {
-                        updatedTheme.colors[_mode as "dark" | "light"]![
-                            _type as keyof GoldRushThemeColorType
-                        ] = value;
-                    });
-                });
-            }
-
-            applyThemeHandler(updatedTheme);
+        (updateTheme: Partial<GoldRushThemeType>) => {
+            const updatedTheme: GoldRushThemeType = defaultsDeep(
+                updateTheme,
+                theme
+            );
+            setTheme(updatedTheme);
         },
         [theme]
-    );
-
-    const applyThemeHandler = useCallback(
-        ({ borderRadius, colors, mode, style }: GoldRushThemeType) => {
-            const body = document.body;
-            const root = document.documentElement;
-
-            switch (mode) {
-                case "dark": {
-                    body.classList.add("dark");
-                    root.classList.add("dark");
-                    break;
-                }
-                case "light": {
-                    body.classList.remove("dark");
-                    root.classList.remove("dark");
-                    break;
-                }
-            }
-
-            switch (style) {
-                case "neo": {
-                    body.classList.add("neo");
-                    root.classList.add("neo");
-                    break;
-                }
-                case "classic": {
-                    body.classList.remove("neo");
-                    root.classList.remove("neo");
-                    break;
-                }
-            }
-
-            root.style.setProperty("--grk-border-radius", `${borderRadius}px`);
-
-            Object.entries(colors).forEach(([_mode, _types]) => {
-                Object.entries(_types).forEach(([_type, value]) => {
-                    if (_type === "primary") {
-                        const shades = primaryShades(
-                            value,
-                            _mode as GoldRushThemeType["mode"]
-                        );
-                        Object.entries(shades).forEach(([shade, color]) => {
-                            root.style.setProperty(
-                                `--grk-${_type}-${_mode}-${shade}`,
-                                color
-                            );
-                        });
-                    } else {
-                        root.style.setProperty(
-                            `--grk-${_type}-${_mode}`,
-                            value
-                        );
-                    }
-                });
-            });
-
-            const _theme: GoldRushThemeType = {
-                borderRadius,
-                colors,
-                mode,
-                style,
-            };
-
-            setTheme(_theme);
-            localStorage.setItem("goldrush_theme", JSON.stringify(_theme));
-        },
-        []
     );
 
     const resetThemeHandler = useCallback(() => {
