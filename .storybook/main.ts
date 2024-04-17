c99605945cb97fd6b53ce7102947c0c0aaa87f92
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2024-04-17 22:08:13 +0530

    chore: update deps

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,6 @@ import { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
     stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
     addons: [
-        "@storybook/addon-onboarding",
         "@storybook/addon-links",
         "@storybook/addon-essentials",
         "@storybook/addon-interactions",
