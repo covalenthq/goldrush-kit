de5da1661fdc9be8f58d0ca6aed61c4b27adec63
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2024-04-23 15:31:00 +0530

    fix: css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -162,41 +162,26 @@ const config = {
             pattern: RegExp(
                 /^(bg|text|shadow|border|stroke|ring|fill)-(background|foreground|secondary)-(light|dark)$/
             ),
-            variants: ["hover", "ui-selected"],
+            variants: [
+                "dark",
+                "hover",
+                "ui-selected",
+                "dark:hover",
+                "dark:ui-selected",
+            ],
         },
         {
             pattern: RegExp(
                 /^(bg|text|shadow|border|stroke|ring|fill)-(primary)-(light|dark)-(DEFAULT|100|200|300|400|500|600|700|800|900)$/
             ),
-            variants: ["hover", "ui-selected"],
+            variants: [
+                "dark",
+                "hover",
+                "ui-selected",
+                "dark:hover",
+                "dark:ui-selected",
+            ],
         },
-        // {
-        //     pattern:
-        //         /^(bg-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-        //     variants: ["hover", "ui-selected"],
-        // },
-        // {
-        //     pattern:
-        //         /^(text-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-        //     variants: ["hover", "ui-selected"],
-        // },
-        // {
-        //     pattern:
-        //         /^(border-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-        //     variants: ["hover", "ui-selected"],
-        // },
-        // {
-        //     pattern:
-        //         /^(ring-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-        // },
-        // {
-        //     pattern:
-        //         /^(stroke-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-        // },
-        // {
-        //     pattern:
-        //         /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
-        // },
     ],
     plugins: [
         require("tailwindcss-animate"),
