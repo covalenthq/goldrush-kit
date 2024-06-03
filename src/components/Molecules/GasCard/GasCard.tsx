940b74c477cc808a722b847edb643cb9e47e2b6e
Karan Pargal <karanpargal007@gmail.com>
2024-06-03 10:30:09 +0530

    fix: responsive

--- src/components/Molecules/GasCard/GasCard.tsx
+++ src/components/Molecules/GasCard/GasCard.tsx
@@ -116,7 +116,7 @@ export const GasCard: React.FC<GasCardProps> = ({ chain_name }) => {
     );
 
     return (
-        <Card className="mt-4 flex w-full flex-col items-center justify-between gap-4 p-4 md:flex-row">
+        <Card className="flex w-full flex-col items-start justify-between gap-4 p-4 md:flex-row md:items-center">
             {maybeResult.match({
                 None: () => (
                     <>
