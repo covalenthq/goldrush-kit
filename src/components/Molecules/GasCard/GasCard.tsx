4f5095c74504db97d91c977a389ad960eb19a959
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2024-06-16 16:25:38 +0530

    fix: gas card default selection

--- src/components/Molecules/GasCard/GasCard.tsx
+++ src/components/Molecules/GasCard/GasCard.tsx
@@ -14,7 +14,7 @@ import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 
 export const GasCard: React.FC<GasCardProps> = ({ chain_name }) => {
-    const [isErc20, setIsErc20] = useState<boolean>(false);
+    const [isErc20, setIsErc20] = useState<boolean>(true);
     const [maybeResult, setMaybeResult] = useState<
         Option<{
             erc: GasPricesResponse;
