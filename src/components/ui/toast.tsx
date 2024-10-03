ceb0f98a95f4be71745edaefb830fdc94872557d
Karan Pargal <83785975+karanpargal@users.noreply.github.com>
2024-10-03 22:12:47 +0530

    fix: Remove cva (#208)

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -21,20 +21,24 @@ const ToastViewport = React.forwardRef<
 ));
 ToastViewport.displayName = ToastPrimitives.Viewport.displayName;
 
-const toastVariants = cva(
-    "group pointer-events-auto relative flex w-full items-center justify-between space-x-2 overflow-hidden rounded border p-4 pr-6 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full data-[state=open]:sm:slide-in-from-bottom-full",
-    {
-        variants: {
-            variant: {
-                default:
-                    "border bg-background-light dark:bg-background-dark text-foreground-light dark:text-foreground-dark",
-            },
-        },
-        defaultVariants: {
-            variant: "default",
-        },
-    },
-);
+const toastVariants = ({
+    variant = "default",
+    className = "",
+}: {
+    variant?: "default";
+    className?: string;
+}): string => {
+    const variants: Record<string, string> = {
+        default:
+            "border bg-background-light dark:bg-background-dark text-foreground-light dark:text-foreground-dark",
+    };
+
+    return cn(
+        "group pointer-events-auto relative flex w-full items-center justify-between space-x-2 overflow-hidden rounded border p-4 pr-6 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full data-[state=open]:sm:slide-in-from-bottom-full",
+        variants[variant],
+        className,
+    );
+};
 
 const Toast = React.forwardRef<
     React.ElementRef<typeof ToastPrimitives.Root>,
