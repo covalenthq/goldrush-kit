bcb834d2261b2609bed2496515c988a90fe196f3
Yashvardhan Jagnani <60016972+jagnani73@users.noreply.github.com>
2024-10-17 02:41:15 +0530

    feat(address activity): chain change (#213)

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/utils/functions";
-import { cva, type VariantProps } from "class-variance-authority";
+import { type VariantProps } from "class-variance-authority";
 import * as React from "react";
 
 const badgeVariants = ({
