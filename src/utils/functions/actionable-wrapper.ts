e9d88543594c421b653d05e983857b511660d239
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2024-10-23 19:01:33 +0530

    wip-fix: actionables type

--- src/utils/functions/actionable-wrapper.ts
+++ src/utils/functions/actionable-wrapper.ts
@@ -1,9 +1,7 @@
 import { type ActionableType } from "../types/shared.types";
-import { createElement } from "react";
+import { createElement, type ElementType } from "react";
 
-export const actionableWrapper = <
-    T extends keyof JSX.IntrinsicElements | React.ComponentType<any>,
->(
+export const actionableWrapper = <T extends ElementType>(
     actionable: ActionableType<T> | null,
     children: React.ReactNode,
 ): React.ReactNode => {
